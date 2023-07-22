508f4b98172fe6444ec47ca5e656ee957d43e070
bradfiller64 <fillerbrad64@gmail.com>
2023-07-22 13:06:16 -0700

    app.ts

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response, NextFunction } from 'express'
+import morgan from 'morgan';
+
+const app = express();
+
+
+// Set up morgan as middleware to log HTTP requests
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const cors = require('cors');
+const corsOptions = {
+    origin: ['http://localhost:3001']
+};
+app.use(cors(corsOptions));
+
+// routes
+app.use('/messages', messageRoutes)
+app.use('/items', itemRoutes);
+app.use('/users', userRoutes);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(404).end();
+});
+
+app.listen(3000);
