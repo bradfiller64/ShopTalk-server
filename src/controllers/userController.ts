4f0dfc42d55d35c91f26188f08634204e51860cb
bradfiller64 <fillerbrad64@gmail.com>
2023-09-23 16:24:44 -0700

    userRoute & userController set up. Now working on item

---
+++ src/controllers/userController.ts
@@ -0,0 +1,122 @@
+import { RequestHandler } from "express";
+import { User } from "../models/user";
+import { comparePasswords, hashPassword, signUserToken, verifyUser } from "../services/auth";
+
+export const getAllUsers: RequestHandler = async (req, res, next) => {
+    let users = await User.findAll();
+    res.status(200).json(users);
+}
+
+export const createUser: RequestHandler = async (req, res, next) => {
+    let newUser: User = req.body;
+
+    // checking for all required fields filled out
+    if (newUser.username && newUser.password && newUser.email && newUser.city && newUser.state) {
+        let hashedPassword = await hashPassword(newUser.password);
+
+        //sets default avatar if none is provided
+        newUser.password = hashedPassword;
+        if (newUser.avatar?.length < 1) {
+            newUser.avatar = 'https://akns-images.eonline.com/eol_images/Entire_Site/2018024/rs_600x600-180124163953-600-tom-myspace.jpg?fit=around%7C1200:1200&output-quality=90&crop=1200:1200;center,top'
+        }
+
+        let created = await User.create(newUser);
+
+        res.status(200).json({
+            username: created.username,
+            userId: created.userId
+        });
+    }
+    else {
+        res.status(400).send('Username and password required!');
+    }
+}
+
+export const getUser: RequestHandler = async (req, res, next) => {
+    let username = req.params.username;
+
+    let user = await User.findByPk(username);
+
+    // checking for a user
+    if (user) {
+        res.status(200).json(user);
+    }
+    else {
+        res.status(404).json({});
+    }
+}
+
+export const updateUser: RequestHandler = async (req, res, next) => {
+    // user authentication
+    let user: User | null = await verifyUser(req);
+
+    if (!user) {
+        return res.status(403).send();
+    }
+
+    let username = req.params.username;
+    let newUser: User = req.body;
+    newUser.username = user.username;
+
+    let userFound = await User.findByPk(username);
+
+    // checking for found user and username found matches current user
+    if (userFound && userFound.username == newUser.username) {
+        await User.update(newUser, {
+            where: { username: username }
+
+        });
+        res.status(200).json();
+    }
+    else {
+        res.status(400).json();
+    }
+
+}
+
+export const deleteUser: RequestHandler = async (req, res, next) => {
+    // user authentication
+    let user: User | null = await verifyUser(req);
+
+    if (!user) {
+        return res.status(403).send();
+    }
+
+    let username = req.params.username;
+    let userFound = await User.findByPk(username);
+
+    // checking if user was found and matches current user
+    if (userFound && userFound.username == user.username) {
+        await User.destroy({
+            where: { username: username }
+        });
+        res.status(200).json();
+    }
+    else {
+        res.status(404).json();
+    }
+}
+
+export const loginUser: RequestHandler = async (req, res, next) => {
+    // Look up user by their username
+    let existingUser: User | null = await User.findOne({
+        where: { username: req.body.username }
+    });
+
+    // If user exists, check that password matches
+    if (existingUser) {
+        let passwordsMatch = await comparePasswords(req.body.password, existingUser.password);
+
+        // If passwords match, create a JWT
+        if (passwordsMatch) {
+            let token = await signUserToken(existingUser);
+            res.status(200).json({ token, user: existingUser });
+        }
+        else {
+            res.status(401).json('Invalid password!');
+        }
+    }
+    else {
+        res.status(401).json('Invalid username!');
+    }
+}
---
+++ src/routes/itemRoutes.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import {
+    getItemById,
+    createItem,
+    updateItem,
+    deleteItem,
+    getItemsByUser,
+} from '../controllers/itemController';
+
+const router = express.Router();
+
+// Get item by ID
+router.get('/:id', getItemById);
+
+// Create a new item
+router.post('/', createItem);
+
+// Update item by ID
+router.put('/:id', updateItem);
+
+// Delete item by ID
+router.delete('/:id', deleteItem);
+
+// Get items by user
+router.get('/user/:username', getItemsByUser);
+
+export default router;
